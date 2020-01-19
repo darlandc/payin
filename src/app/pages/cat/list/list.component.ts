93424d2e345bb04e69465a99a397ab307b0f9045
Darlan de Castro <darlandc@gmail.com>
2020-01-19 02:08:47 -0300

    fix logo

--- src/app/pages/cat/list/list.component.ts
+++ src/app/pages/cat/list/list.component.ts
@@ -6,14 +6,16 @@ export interface TiposCategorias {
   registros: number;
   gastosTotais: number;
   porcentagem: string;
-  actions: any;
+  actions: Array<string>;
 }
 
-const ELEMENT_DATA: TiposCategorias[] = [
-  { nome: 'Alimentação', registros: 120, gastosTotais: 34593, porcentagem: '20%', actions: "['editar', 'deletar']" },
-  { nome: 'Bebidas', registros: 209, gastosTotais: 40026, porcentagem: '30%', actions: "['editar', 'deletar']" }
-];
+const editar = 'Editar';
+const excluir = 'Excluir';
 
+const ELEMENT_DATA: TiposCategorias[] = [
+  { nome: 'Alimentação', registros: 120, gastosTotais: 34593, porcentagem: '20%', actions: [editar + excluir] } ,
+  { nome: 'Bebidas', registros: 209, gastosTotais: 40026, porcentagem: '30%', actions: [editar + excluir] }
+];
 
 @Component({
   selector: 'app-list',
@@ -21,8 +23,12 @@ const ELEMENT_DATA: TiposCategorias[] = [
   styleUrls: ['./list.component.sass']
 })
 export class ListComponent implements OnInit {
+  actions: any;
+  editar = ELEMENT_DATA
+
 
   constructor() {
+    console.log(ELEMENT_DATA);
   }
 
   panelOpenState = false;
@@ -36,6 +42,7 @@ export class ListComponent implements OnInit {
   }
 
   ngOnInit() {
+
 
   }
 
